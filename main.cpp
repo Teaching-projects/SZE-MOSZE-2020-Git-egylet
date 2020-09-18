ce4cba504cce87d5d38923a3e39e3fdbba4e7a8f
mrpig2000 <adorjanandris2000@gmail.com>
2020-09-18 15:59:30 +0200

    Main bugfix

--- main.cpp
+++ main.cpp
@@ -4,33 +4,29 @@
 
 
 int main(int argc, char* argv[]) {
-    character A(argv[1], std::stoi(argv[2]), std::stoi(argv[3]));
-    character B(argv[4], std::stoi(argv[5]), std::stoi(argv[6]));
+    character player1(argv[1], std::stoi(argv[2]), std::stoi(argv[3]));
+    character player2(argv[4], std::stoi(argv[5]), std::stoi(argv[6]));
 
-    A.status();
-    B.status();
-    while (A.isAlive() && B.isAlive()) {
+    player1.status();
+    player2.status();
+    while (player1.isAlive() && player2.isAlive()) {
 
-        A.attack(B);
-        A.status();
-        B.status();
-        if (!B.isAlive()) {
+        player1.attack(player2);
+        player1.status();
+        player2.status();
+        if (!player2.isAlive()) {
             std::cout << argv[4] << " died. " << argv[1] << " wins." << std::endl;
             break;
         }
 
-        B.attack(A);
-        A.status();
-        B.status();
-        if (!A.isAlive()) {
+        player2.attack(player1);
+        player1.status();
+        player2.status();
+        if (!player1.isAlive()) {
             std::cout << argv[1] << " died. " << argv[4] << " wins." << std::endl;
             break;
         }
 
     }
-    
-
-
-
-
+    delete player1, player2;
 }
