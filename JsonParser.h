8bfb8c5ee3be8b0cef1f473e75555c3289726710
kokohun <natzkornel@gmail.com>
2020-10-13 21:27:58 +0200

    Changed functions to static in JsonParser.h file

--- JsonParser.h
+++ JsonParser.h
@@ -3,12 +3,15 @@
 #include <iostream>
 #include <fstream>
 
-class parser
+class Parser
 {
 
 public:
 
     static std::map <std::string, std::string> jsonParser(std::istream& file);
     static std::map <std::string, std::string> jsonParser(std::string file);
+    static std::map <std::string, std::string> jsonParserFileName(std::string filename);
+    static std::map <std::string, std::string> jsonParserString(std::string str);
+
 
 };
