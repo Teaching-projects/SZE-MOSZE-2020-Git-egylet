53222c40a4dad78305a3aa211bb7e1cdb43d98a2
kokohun <natzkornel@gmail.com>
2020-10-13 21:25:44 +0200

    Take apart JsonParser functions

--- JsonParser.cpp
+++ JsonParser.cpp
@@ -1,6 +1,6 @@
 #include "JsonParser.h"
 
-std::map <std::string, std::string> parser::jsonParser(std::istream& file) {
+std::map <std::string, std::string> Parser::jsonParser(std::istream& file) {
     std::string content, line;
 
     while (std::getline(file, line))
@@ -11,32 +11,37 @@ std::map <std::string, std::string> parser::jsonParser(std::istream& file) {
     return jsonParser(content);
 }
 
-std::map <std::string, std::string> parser::jsonParser(std::string str) {
+std::map <std::string, std::string> Parser::jsonParserString(std::string str) {
     std::regex reg("\\s*\"([\\w]*)\"\\s*:\\s*\"?([\\w\\.]*)\"?\\s*[,}]\\s*");
-    std::regex freg("([\\w]*).json$");
     std::map<std::string, std::string> values;
     std::smatch match;
 
-    if (!std::regex_search(str, match, freg))
-    {
-        while (std::regex_search(str, match, reg))
-        {
-            values[match[1]] = match[2];
-            str = match.suffix().str();
-        }
-        return values;
-    }
-    else
-    {
-        std::ifstream file;
-        file.open(str);
+	while (std::regex_search(str, match, reg))
+	{
+		values[match[1]] = match[2];
+		str = match.suffix().str();
+	}
+	return values;
+}
 
-        if (!file.fail()) 
-        {
-            values = jsonParser(file);
-            return values;
-            file.close();
-        }
-        else throw "it does not exist";
-    }
+std::map <std::string, std::string> Parser::jsonParserFileName(std::string filename) {
+	std::ifstream file;
+	file.open(filename);
+
+	if (!file.fail()) 
+	{
+		std::map <std::string, std::string> values = jsonParser(file);
+		return values;
+		
+		file.close();
+	}
+	else throw "it does not exist";
+}
+
+std::map <std::string, std::string> Parser::jsonParser(std::string fileNameOrString) {
+    std::regex freg("([\\w]*).json$");
+    std::smatch match;
+
+    if (!std::regex_search(fileNameOrString, match, freg)) return jsonParserString(fileNameOrString);
+    else return jsonParserFileName(fileNameOrString);
 }
