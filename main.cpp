59318cb6f5ffe5bea973fc461af91fb117de790e
kokohun <natzkornel@gmail.com>
2020-10-29 17:29:11 +0100

    Changed final main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,72 @@
-#include "Character.h"
-#include "Player.h"
-#include <iostream>
-#include <fstream>
-
-int main(int argc, char* argv[]) {
-    try {
-        if (argc == 3) {
-			/**
-			\brief The case of normal input.
-			*/
-            Player player1 = Player::parseUnit(argv[1] /** [in] first player's .json file*/);		///< Making the first player by the data from the .json file
-            Character player2 = Character::parseUnit(argv[2] /** [in] second player's .json file*/);	///< Making the second player by the data from the .json file
-
-            std::string winner = player1.attack(player1 /** the player from the first .json file*/, player2 /** the player from the second .json file*/);	///< Calculating the results
-			      std::cout << winner << std::endl;	///< Write out the results on the screen
-        }
-        else {
-			/**
-			\brief The case of wrong input.
-			*/
-            std::cout << "Inappropriate command line inputs. Game will now close." << std::endl;	///< Write out the problem on the screen
-            std::cout << "Input should look like: 'A.json B.json'" << std::endl;	///< Write out the correct input form on the screen
-            return 1;
-        }
-
+    #include <iostream>
+    #include <map>
+    #include <string>
+    #include <filesystem>
+    #include <sstream>
+    #include <algorithm>
+    #include <iterator>
+    #include <list>
+     
+    #include "JSON.h"
+    #include "Hero.h"
+    #include "Monster.h"
+     
+     
+     
+     
+    const std::map<int,std::string> error_messages = {
+        { 1 , "Bad number of arguments. Only a single scenario file should be provided." },
+        { 2 , "The provided scenario file is not accessible." },
+        { 3 , "The provided scenario file is invalid." },
+        { 4 , "JSON parsing error." }
+    };
+     
+    void bad_exit(int exitcode){
+        std::cerr 
+            << (error_messages.count(exitcode) ? error_messages.at(exitcode) : "Unknown error")
+            << std::endl;
+        exit(exitcode);
     }
-    catch(std::exception & e) {
-		/**
-		\brief The case of wrong input or missing file(s).
-		*/
-        std::cout << "Inappropriate command line inputs, or the file does not exist,please try again" << std::endl;	///< Write out the problem on the screen
-        std::cout << "Input should look like: 'A.json B.json'" << std::endl;	///< Write out the correct input form on the screen
-        return 1;
+     
+    int main(int argc, char** argv){
+        if (argc != 2) bad_exit(1);
+        if (!std::filesystem::exists(argv[1])) bad_exit(2);
+     
+        std::string hero_file;
+        std::list<std::string> monster_files;
+        try {
+            JSON scenario = JSON::parseFromFile(argv[1]); 
+            if (!(scenario.count("hero")&&scenario.count("monsters"))) bad_exit(3);
+            else {
+                hero_file=scenario.get<std::string>("hero");
+                std::istringstream monsters(scenario.get<std::string>("monsters"));
+                std::copy(std::istream_iterator<std::string>(monsters),
+                    std::istream_iterator<std::string>(),
+                    std::back_inserter(monster_files));
+            }
+        } catch (const JSON::ParseException& e) {bad_exit(4);}
+     
+        try { 
+            Hero hero{Hero::parse(hero_file)};
+            std::list<Monster> monsters;
+            for (const auto& monster_file : monster_files)
+                monsters.push_back(Monster::parse(monster_file));        
+     
+            while (hero.isAlive() && !monsters.empty()) {
+                std::cout 
+                    << hero.getName() << "(" << hero.getLevel()<<")"
+                    << " vs "
+                    << monsters.front().getName()
+                    <<std::endl;
+                hero.fightTilDeath(monsters.front());
+                if (!monsters.front().isAlive()) monsters.pop_front();
+            }
+            std::cout << (hero.isAlive() ? "The hero won." : "The hero died.") << std::endl;
+            std::cout << hero.getName() << ": LVL" << hero.getLevel() << std::endl
+                      << "   HP: "<<hero.getHealthPoints()<<"/"<<hero.getMaxHealthPoints()<<std::endl
+                      << "  DMG: "<<hero.getDamage()<<std::endl
+                      << "  ACD: "<<hero.getAttackCoolDown()<<std::endl
+                      ;
+        } catch (const JSON::ParseException& e) {bad_exit(4);}
+        return 0;
     }
-    return 0;
-}
