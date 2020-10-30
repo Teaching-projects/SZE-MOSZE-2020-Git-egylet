b1e00862904754fe01c206fa7dba81193b4e38db
mrpig2000 <adorjanandris2000@gmail.com>
2020-10-30 12:21:17 -0700

    Fixing gtests.sh 02

--- tests/gtests.sh
+++ tests/gtests.sh
@@ -18,4 +18,5 @@ sudo ln -st /usr/lib/ /usr/src/gtest/libgtest_main.a
 cd -
 cmake CMakeLists.txt
 make
-chmod +x runTests && ./runTests $i $j >> gtests_output.txt
+chmod +x runTests
+./runTests $i $j >> gtests_output.txt
