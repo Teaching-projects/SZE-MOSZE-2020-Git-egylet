ae4eba7059f6f7cdc78ed42a9ff554aea7cec568
mrpig2000 <adorjanandris2000@gmail.com>
2020-10-30 11:57:41 -0700

    Add gtests.sh, gtests_good_output.txt and gtestsflow.yml

---
+++ tests/gtests.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd tests
+
+if test -f gtests_output.txt
+then
+	rm gtests_output.txt
+fi
+
+sudo apt install build-essential
+sudo apt install libgtest-dev
+sudo apt install cmake
+cd /usr/src/gtest
+sudo cmake CMakeLists.txt
+sudo make
+sudo ln -st /usr/lib/ /usr/src/gtest/libgtest.a
+sudo ln -st /usr/lib/ /usr/src/gtest/libgtest_main.a
+cd -
+cmake CMakeLists.txt
+make
+./runTests $i $j >> gtests_output.txt
