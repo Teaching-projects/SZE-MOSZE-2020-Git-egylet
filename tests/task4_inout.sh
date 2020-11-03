ccb85b15be66ae50dd5382ab45ed91b626bc69ae
Áron Kisszölgyémi <aron.kisszolgyemi@drem.hu>
2020-11-03 18:05:51 +0100

    Final fixes

--- tests/task4_inout.sh
+++ tests/task4_inout.sh
@@ -12,3 +12,6 @@ fi
 
 ./a.out scenarios/scenario1.json >> native_output1.txt
 ./a.out scenarios/scenario2.json >> native_output2.txt
+
+diff native_output1.txt scenarios/output1.txt
+diff native_output2.txt scenarios/output2.txt
