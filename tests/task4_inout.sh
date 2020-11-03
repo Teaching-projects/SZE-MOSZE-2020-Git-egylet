52d0cb9a0e1b76884b2415d149645a652b64aeb2
kokohun <natzkornel@gmail.com>
2020-11-03 13:59:49 +0100

    Refactor task4_inout.sh

--- tests/task4_inout.sh
+++ tests/task4_inout.sh
@@ -10,10 +10,5 @@ then
 	rm scenario2_native.txt
 fi
 
-for i in scenarios/*.json
-do
-	./a.out $i >> ${i%.json}_native.txt
-done
-
-#~ ./a.out scenarios/scenario1.json >> native_output1.txt
-#~ ./a.out scenarios/scenario2.json >> native_output2.txt
+./a.out scenarios/scenario1.json >> native_output1.txt
+./a.out scenarios/scenario2.json >> native_output2.txt
