67e6cd077d22dd56a911e6efcff9d7b299bbddaa
kokohun <natzkornel@gmail.com>
2020-11-03 12:59:42 +0100

    Refreshed g++/gcc latest version

--- tests/task4_inout.sh
+++ tests/task4_inout.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-./a.out scenarios/scenario1.json
-./a.out scenarios/scenario2.json
+if test -f native_output1.txt
+then
+	rm native_output1.txt
+fi
+
+if test -f native_output2.txt
+then
+	rm native_output2.txt
+fi
+
+./a.out scenarios/scenario1.json >> native_output1.txt
+./a.out scenarios/scenario2.json >> native_output2.txt
