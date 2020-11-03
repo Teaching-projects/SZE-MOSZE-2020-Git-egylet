f606561c6cbaf35b4554ccc80353ca30b5f5cc3d
kokohun <natzkornel@gmail.com>
2020-11-03 12:48:03 +0100

    Updated task4_inout.sh lines #2

--- tests/task4_inout.sh
+++ tests/task4_inout.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-if test -f native_output1.txt
-then
-	rm native_output1.txt
-fi
-
-if test -f native_output2.txt
-then
-	rm native_output2.txt
-fi
-
-./a.out scenarios/scenario1.json >> native_output1.txt
-./a.out scenarios/scenario2.json >> native_output2.txt
+./a.out scenarios/scenario1.json
+./a.out scenarios/scenario2.json
