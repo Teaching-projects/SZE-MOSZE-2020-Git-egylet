7d8ca307574e0f072893f4d97b81e7bcd7f2cd14
Áron Kisszölgyémi <aron.kisszolgyemi@drem.hu>
2020-11-05 10:53:57 +0100

    Fixing gtests.sh#4

--- tests/gtests.sh
+++ tests/gtests.sh
@@ -9,9 +9,10 @@ sudo apt install software-properties-common
 sudo apt install gcc-7 g++-7 gcc-8 g++-8 gcc-9 g++-9
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 90 --slave /usr/bin/g++ g++ /usr/bin/g++-9 --slave /usr/bin/gcov gcov /usr/bin/gcov-9
 
-cd /usr/src/gtest
+
+cd tests
 sudo cmake .
 sudo make
 
-cd -
+cd /usr/src/gtest
 ./runTests
