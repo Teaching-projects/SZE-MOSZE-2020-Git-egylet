b856f3fcc1c1b9deab4d91918ae66981b23fd034
kokohun <natzkornel@gmail.com>
2020-11-18 17:56:50 +0100

    Added Map class with loading map data and wall analyzer functions

---
+++ Map.h
@@ -0,0 +1,25 @@
+#include <string>
+#include <fstream>
+#include <vector>
+
+class Map {
+	
+private:
+	std::vector<std::string> map;
+	
+public:
+	Map (std::string& filename);
+	
+	enum type
+	{
+		Free, Wall
+	};
+	
+	Map::type get(int x, int y) const;
+	
+	class WrongIndexException : public std::runtime_error
+    {
+    public:
+        explicit WrongIndexException(const std::string &content) : std::runtime_error(content) {}
+    };
+};
