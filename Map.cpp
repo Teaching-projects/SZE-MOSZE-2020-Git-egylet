8ec6797df9f616fdeac3997abc24e9a744e3d759
kokohun <natzkornel@gmail.com>
2020-11-20 11:16:44 +0100

    Added doxygen comments to Map.h, Map.cpp

--- Map.cpp
+++ Map.cpp
@@ -1,7 +1,17 @@
+/**
+\mainpage Git egylet's documentation
+\class Map
+This is the description of Map.cpp
+\version 1.0
+\author kokohun
+\date 2020/11/20 11:11
+Created on: 2020/11/20 11:11
+*/
+
 #include "Map.h"
 #include <fstream>
 
-Map::Map(const std::string& filename) {
+Map::Map(const std::string& filename) ///< Reads map from file {
 	std::ifstream file(filename);
 	if (!file.fail()) 
 	{
@@ -16,7 +26,7 @@ Map::Map(const std::string& filename) {
 	file.close();
 }
 
-Map::type Map::get(int x, int y) const {
+Map::type Map::get(int x, int y) const ///< Get map field type {
 	if (y >= map.size() || y < 0) throw WrongIndexException("y coordinate is out of range");
 	if (x >= map[y].length() || x < 0) throw WrongIndexException("x coorfinate is out of range");
 
--- Map.h
+++ Map.h
@@ -1,3 +1,13 @@
+/**
+\mainpage Git egylet's documentation
+\class Map
+This is the description of Map.h
+\version 1.0
+\author kokohun
+\date 2020/11/20 10:56
+Created on: 2020/11/20 10:56
+*/
+
 #include <string>
 #include <fstream>
 #include <vector>
@@ -5,19 +15,19 @@
 class Map {
 	
 private:
-	std::vector<std::string> map;
+	std::vector<std::string> map; ///< The map data array
 	
 public:
-	Map(const std::string& filename);
+	Map(const std::string& filename); ///< Map class constructor, which reads map from file
 	
 	enum type
 	{
-		Free, Wall
+		Free, Wall /** Map field types*/
 	};
 	
-	Map::type get(int x, int y) const;
+	Map::type get(int x, int y) const; ///< Get map field type
 	
-	class WrongIndexException : public std::runtime_error
+	class WrongIndexException : public std::runtime_error; ///< Map class Exception function
     {
     public:
         explicit WrongIndexException(const std::string &content) : std::runtime_error(content) {}
