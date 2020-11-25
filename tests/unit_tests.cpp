3b058597792e0d2474111d7e17096ca75c0c1136
mrpig2000 <adorjanandris2000@gmail.com>
2020-11-25 21:58:08 +0100

    Fixing existing tests in unit_tests.cpp

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -1,6 +1,8 @@
 #include "../Monster.h"
+#include "../Hero.h"
 #include "../JSON.h"
 #include "../Map.h"
+#include "../Damage.h"
 #include <gtest/gtest.h>
 
 
@@ -15,6 +17,7 @@ TEST(JSON, jsonParser) {
 	ASSERT_EQ(values.get<std::string>("name"),"Kakarott");
 	ASSERT_EQ(values.get<int>("health_points"),300);
 	ASSERT_EQ(values.get<int>("damage"),150);
+	ASSERT_EQ(values.get<int>("magical-damage"),40);
 	ASSERT_EQ(values.get<double>("attack_cooldown"),5.5);
 	ASSERT_EQ(values.get<double>("defense"),240.0);
 }
@@ -33,6 +36,7 @@ TEST(JSON, parseFromString) {
 	ASSERT_EQ(values.get<std::string>("name"),"Sally");
 	ASSERT_EQ(values.get<int>("health_points"),500);
 	ASSERT_EQ(values.get<int>("damage"),250);
+	ASSERT_EQ(values.get<int>("magical-damage"),25);
 	ASSERT_EQ(values.get<double>("attack_cooldown"),3.5);
 	ASSERT_EQ(values.get<double>("defense"),180.0);
 }
@@ -44,6 +48,7 @@ TEST(JSON, parseFromFile) {
 	ASSERT_EQ(values.get<std::string>("name"),"Maple");
 	ASSERT_EQ(values.get<int>("health_points"),700);
 	ASSERT_EQ(values.get<int>("damage"),100);
+	ASSERT_EQ(values.get<int>("magical-damage"),20);
 	ASSERT_EQ(values.get<double>("attack_cooldown"),4.5);
 	ASSERT_EQ(values.get<double>("defense"),120.0);
 }
@@ -59,6 +64,7 @@ TEST(JSON, Fallen) {
     ASSERT_EQ(values.get<std::string>("name"),"Fallen");
 	ASSERT_EQ(values.get<int>("health_points"),4);
 	ASSERT_EQ(values.get<int>("damage"),2);
+	ASSERT_EQ(values.get<int>("magical-damage"),1);
 	ASSERT_EQ(values.get<double>("attack_cooldown"),1.6);
 	ASSERT_EQ(values.get<double>("defense"),1.1);
 }
@@ -74,6 +80,7 @@ TEST(JSON, Mixi) {
 	ASSERT_EQ(values.get<std::string>("name"),"Mixi");
 	ASSERT_EQ(values.get<int>("health_points"),320);
 	ASSERT_EQ(values.get<int>("damage"),140);
+	ASSERT_EQ(values.get<int>("magical-damage"),100);
 	ASSERT_EQ(values.get<double>("attack_cooldown"),5.3);
 	ASSERT_EQ(values.get<double>("defense"),280.0);
 }
@@ -89,6 +96,7 @@ TEST(JSON, Spacy) {
 	ASSERT_EQ(values.get<std::string>("name"),"Spacy");
 	ASSERT_EQ(values.get<int>("health_points"),400);
 	ASSERT_EQ(values.get<int>("damage"),250);
+	ASSERT_EQ(values.get<int>("magical-damage"),30);
 	ASSERT_EQ(values.get<double>("attack_cooldown"),6.5);
 	ASSERT_EQ(values.get<double>("defense"),100.0);
 }
@@ -104,80 +112,74 @@ TEST(JSON, Mixpacy) {
     ASSERT_EQ(values.get<std::string>("name"),"Mixpacy");
 	ASSERT_EQ(values.get<int>("health_points"),456);
 	ASSERT_EQ(values.get<int>("damage"),259);
+	ASSERT_EQ(values.get<int>("magical-damage"),69);
 	ASSERT_EQ(values.get<double>("attack_cooldown"),6.9);
 	ASSERT_EQ(values.get<double>("defense"),233.0);
 }
 
-/* Direkt rossz test, a workflow rosszul fut le tole
-TEST(JsonParser, errorfile) {
-	std::ifstream file;
-	file.open("../units/error/Player_4_Enigma.json");
-	std::map<std::string, std::string> actual = Parser::jsonParser(file);
-	std::map<std::string, std::string> whichis
-	{
-		{"name", "Enigma"},
-		{"hp", "3000"},
-		{"dmg", "300"},
-		{"acd", "5.0"}
-	};
-	
-	for (auto entry : actual)
-	{
-		ASSERT_EQ(whichis[entry.first], entry.second);
-	}
-	
-	file.close();
-}
-*/
-
 TEST(Monster, getName){
-	Monster player_test("Kakarott", 300, 150, 5.5, 240);
+	Damage tmp (150, 40);
+	Monster player_test ("Kakarott", 300, tmp, 5.5, 240);
 	std::string test_name = player_test.getName();
 	std::string expected_name = "Kakarott";
 	ASSERT_EQ(expected_name, test_name);
 }
 
 TEST(Monster, getHealthPoints){
-	Monster player_test("Kakarott", 300, 150, 5.5, 240);
+	Damage tmp (150, 40);
+	Monster player_test ("Kakarott", 300, tmp, 5.5, 240);
 	int test_hp = player_test.getHealthPoints();
 	int expected_hp = 300;
 	ASSERT_EQ(expected_hp, test_hp);
 }
 
 TEST(Monster, getDamage){
-	Monster player_test("Kakarott", 300, 150, 5.5, 240);
-	int test_dmg = player_test.getDamage();
-	int expected_dmg = 150;
-	ASSERT_EQ(expected_dmg, test_dmg);
+	Damage tmp (150, 40);
+	Monster player_test ("Kakarott", 300, tmp, 5.5, 240);
+	Damage test_dmg (player_test.getDamage());
+
+	int test_pdmg = test_dmg.getPhysical();
+	int test_mdmg = test_dmg.getMagical();
+
+	int expected_pdmg = 150;
+	int expected_mdmg = 40;
+
+	ASSERT_EQ(expected_pdmg, test_pdmg);
+	ASSERT_EQ(expected_mdmg, test_mdmg);
 }
 
 TEST(Monster, getAttackCoolDown){
-	Monster player_test("Kakarott", 300, 150, 5.5, 240);
+	Damage tmp (150, 40);
+	Monster player_test ("Kakarott", 300, tmp, 5.5, 240);
 	double test_acd = player_test.getAttackCoolDown();
 	double expected_acd = 5.5;
 	ASSERT_EQ(expected_acd, test_acd);
 }
 
 TEST(Monster, getDefense){
-	Monster player_test("Kakkarott", 300, 150, 5.5, 240);
+	Damage tmp (150, 40);
+	Monster player_test ("Kakarott", 300, tmp, 5.5, 240);
 	double test_def = player_test.getDefense();
 	double expected_def = 240;
 	ASSERT_EQ(expected_def, test_def);
 }
 
 TEST(Monster, isAlive){
-	Monster player_test("Kakarott", 300, 150, 5.5, 240);
+	Damage tmp (150, 40);
+	Monster player_test ("Kakarott", 300, tmp, 5.5, 240);
 	bool test = player_test.isAlive();
 	bool expected = true;
 	ASSERT_EQ(expected, test);
 }
 
 TEST(Monster, hit){
-	Monster player_test_1("Kakarott", 300, 150, 5.5, 240);
-	Monster player_test_2("Kakarott_2", 300, 150, 5.5, 120);
+	Damage tmp_1 (150, 40);
+	Monster player_test_1 ("Kakarott", 300, tmp_1, 5.5, 240);
+	Damage tmp_2 (150, 40);
+	Monster player_test_2("Kakarott", 300, tmp_2, 5.5, 120);
 	player_test_1.hit(&player_test_2);
 	int test_hp = player_test_2.getHealthPoints();
-	int expected_hp = 270;
+	int expected_hp = 230;
 	ASSERT_EQ(expected_hp, test_hp);
 }
 
@@ -185,26 +187,32 @@ TEST(Monster, parse){
 	Monster player_test = Monster::parse("../units/Player_1_Kakarott.json");
 	std::string test_name = player_test.getName();
 	int test_hp = player_test.getHealthPoints();
-	int test_dmg = player_test.getDamage();
+	Damage test_dmg = player_test.getDamage();
+	int test_pdmg = test_dmg.getPhysical();
+	int test_mdmg = test_dmg.getMagical();
 	double test_acd = player_test.getAttackCoolDown();
 	double test_def = player_test.getDefense();
 	
 	std::string expected_name = "Kakarott";
 	int expected_hp = 300;
-	int expected_dmg = 150;
+	int expected_pdmg = 150;
+	int expected_mdmg = 40;
 	double expected_acd = 5.5;
 	double expected_def = 240.0;
 	
 	ASSERT_EQ(expected_name, test_name);
 	ASSERT_EQ(expected_hp, test_hp);
-	ASSERT_EQ(expected_dmg, test_dmg);
+	ASSERT_EQ(expected_pdmg, test_pdmg);
+	ASSERT_EQ(expected_mdmg, test_mdmg);
 	ASSERT_DOUBLE_EQ(expected_acd, test_acd);
 	ASSERT_DOUBLE_EQ(expected_def, test_def);
 }
 
 TEST(Monster, fightTilDeath){
-	Monster player_test_1("PLAYER1", 300, 150, 1.5, 240);
-	Monster player_test_2("PLAYER2", 300, 50, 2.5, 120);
+	Damage tmp_1 (150, 40);
+	Monster player_test_1 ("Kakarott", 300, tmp_1, 5.5, 240);
+	Damage tmp_2 (50, 10);
+	Monster player_test_2("Kakarott", 300, tmp_2, 2.5, 120);
 	std::string expected_winner = "Good.";
 	std::string test_winner = "";
 	
