80d436708cce97819aa7884cec2da684fbad01f0
mrpig2000 <adorjanandris2000@gmail.com>
2020-12-01 19:28:39 +0100

    Updated unit tests

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -120,16 +120,22 @@ TEST(JSON, Mixpacy) {
 TEST(Monster, getName){
 	Damage tmp (150, 40);
 	Monster player_test ("Kakarott", 300, tmp, 5.5, 240);
+	
 	std::string test_name = player_test.getName();
+	
 	std::string expected_name = "Kakarott";
+	
 	ASSERT_EQ(expected_name, test_name);
 }
 
 TEST(Monster, getHealthPoints){
 	Damage tmp (150, 40);
 	Monster player_test ("Kakarott", 300, tmp, 5.5, 240);
+	
 	int test_hp = player_test.getHealthPoints();
+	
 	int expected_hp = 300;
+	
 	ASSERT_EQ(expected_hp, test_hp);
 }
 
@@ -151,24 +157,33 @@ TEST(Monster, getDamage){
 TEST(Monster, getAttackCoolDown){
 	Damage tmp (150, 40);
 	Monster player_test ("Kakarott", 300, tmp, 5.5, 240);
+	
 	double test_acd = player_test.getAttackCoolDown();
+	
 	double expected_acd = 5.5;
+	
 	ASSERT_EQ(expected_acd, test_acd);
 }
 
 TEST(Monster, getDefense){
 	Damage tmp (150, 40);
 	Monster player_test ("Kakarott", 300, tmp, 5.5, 240);
+	
 	double test_def = player_test.getDefense();
+	
 	double expected_def = 240;
+	
 	ASSERT_EQ(expected_def, test_def);
 }
 
 TEST(Monster, isAlive){
 	Damage tmp (150, 40);
 	Monster player_test ("Kakarott", 300, tmp, 5.5, 240);
+	
 	bool test = player_test.isAlive();
+	
 	bool expected = true;
+	
 	ASSERT_EQ(expected, test);
 }
 
@@ -177,14 +192,34 @@ TEST(Monster, hit){
 	Monster player_test_1 ("Kakarott", 300, tmp_1, 5.5, 240);
 	Damage tmp_2 (150, 40);
 	Monster player_test_2("Kakarott", 300, tmp_2, 5.5, 120);
+	
 	player_test_1.hit(&player_test_2);
+	
 	int test_hp = player_test_2.getHealthPoints();
+	
 	int expected_hp = 230;
+	
+	ASSERT_EQ(expected_hp, test_hp);
+}
+
+TEST(Monster, getHit){
+	Damage tmp_1 (150, 40);
+	Monster player_test_1 ("Kakarott", 300, tmp_1, 5.5, 240);
+	Damage tmp_2 (150, 40);
+	Monster player_test_2("Kakarott", 300, tmp_2, 5.5, 120);
+	
+	player_test_2.getHit(&player_test_2);
+	
+	int test_hp = player_test_2.getHealthPoints();
+	
+	int expected_hp = 230;
+	
 	ASSERT_EQ(expected_hp, test_hp);
 }
 
 TEST(Monster, parse){
 	Monster player_test = Monster::parse("../units/Player_1_Kakarott.json");
+	
 	std::string test_name = player_test.getName();
 	int test_hp = player_test.getHealthPoints();
 	Damage test_dmg = player_test.getDamage();
@@ -213,6 +248,7 @@ TEST(Monster, fightTilDeath){
 	Monster player_test_1 ("Kakarott", 300, tmp_1, 5.5, 240);
 	Damage tmp_2 (50, 10);
 	Monster player_test_2("Kakarott", 300, tmp_2, 2.5, 120);
+	
 	std::string expected_winner = "Good.";
 	std::string test_winner = "";
 	
@@ -224,43 +260,178 @@ TEST(Monster, fightTilDeath){
 	ASSERT_EQ(expected_winner, test_winner);
 }
 
-/* TODO in update - all HERO functions
+TEST(Hero, getLevel){
+	Damage tmp (3, 1);
+	Hero player_test ("Prince Aidan of Khanduras", 30, tmp, 1.1, 0.5, 20, 5, 1, 0.9, 0.1, 1);
+	
+	int test_level = player_test.getLevel();
+	
+	int expected_level = 1;
+	
+	ASSERT_EQ(expected_level, test_level);
+}
 
-//Hero getLevel
+TEST(Hero, getExperience){
+	Damage tmp (3, 1);
+	Hero player_test ("Prince Aidan of Khanduras", 30, tmp, 1.1, 0.5, 20, 5, 1, 0.9, 0.1, 1);
+	
+	int test_xp = player_test.getExperience();
+	
+	int expected_xp = 0;
+	
+	ASSERT_EQ(expected_xp, test_xp);
+}
 
+TEST(Hero, getMaxHealthPoints){
+	Damage tmp (3, 1);
+	Hero player_test ("Prince Aidan of Khanduras", 30, tmp, 1.1, 0.5, 20, 5, 1, 0.9, 0.1, 1);
+	
+	int test_mhp = player_test.getMaxHealthPoints();
+	
+	int expected_mhp = 30;
+	
+	ASSERT_EQ(expected_mhp, test_mhp);
+}
 
-//Hero getMaxHealthPoints
+TEST(Hero, getExperiencePerLevel){
+	Damage tmp (3, 1);
+	Hero player_test ("Prince Aidan of Khanduras", 30, tmp, 1.1, 0.5, 20, 5, 1, 0.9, 0.1, 1);
+	
+	int test_epl = player_test.getExperiencePerLevel();
+	
+	int expected_epl = 20;
+	
+	ASSERT_EQ(expected_epl, test_epl);
+}
 
+TEST(Hero, getHealthPointBonusPerLevel){
+	Damage tmp (3, 1);
+	Hero player_test ("Prince Aidan of Khanduras", 30, tmp, 1.1, 0.5, 20, 5, 1, 0.9, 0.1, 1);
+	
+	int test_hpbpl = player_test.getHealthPointBonusPerLevel();
+	
+	int expected_hpbpl = 5;
+	
+	ASSERT_EQ(expected_hpbpl, test_hpbpl);
+}
 
-//Hero getExperiencePerLevel
+TEST(Hero, getPhysicalDamageBonusPerLevel){
+	Damage tmp (3, 1);
+	Hero player_test ("Prince Aidan of Khanduras", 30, tmp, 1.1, 0.5, 20, 5, 1, 0.9, 0.1, 1);
+	
+	int test_pdbpl = player_test.getPhysicalDamageBonusPerLevel();
+	
+	int expected_pdbpl = 1;
+	
+	ASSERT_EQ(expected_pdbpl, test_pdbpl);
+}
 
+TEST(Hero, getCooldownMultiplierPerLevel){
+	Damage tmp (3, 1);
+	Hero player_test ("Prince Aidan of Khanduras", 30, tmp, 1.1, 0.5, 20, 5, 1, 0.9, 0.1, 1);
+	
+	int test_cmpl = player_test.getCooldownMultiplierPerLevel();
+	
+	int expected_cmpl = 0.9;
+	
+	ASSERT_DOUBLE_EQ(expected_cmpl, test_cmpl);
+}
 
-//Hero getHealthPointBonusPerLevel
+TEST(Hero, getDefenseBonusPerLevel){
+	Damage tmp (3, 1);
+	Hero player_test ("Prince Aidan of Khanduras", 30, tmp, 1.1, 0.5, 20, 5, 1, 0.9, 0.1, 1);
+	
+	int test_dbpl = player_test.getDefenseBonusPerLevel();
+	
+	int expected_dbpl = 0.1;
+	
+	ASSERT_DOUBLE_EQ(expected_dbpl, test_dbpl);
+}
 
+TEST(Hero, getMagicalDamageBonusPerLevel){
+	Damage tmp (3, 1);
+	Hero player_test ("Prince Aidan of Khanduras", 30, tmp, 1.1, 0.5, 20, 5, 1, 0.9, 0.1, 1);
+	
+	int test_mdbpl = player_test.getMagicalDamageBonusPerLevel();
+	
+	int expected_mdbpl = 1;
+	
+	ASSERT_EQ(expected_mdbpl, test_mdbpl);
+}
 
-//Hero getPhysicalDamageBonusPerLevel
+TEST(Hero, hit_wo_levelup){
+	Damage tmp (3, 1);
+	Hero player_test_1 ("Prince Aidan of Khanduras", 30, tmp, 1.1, 0.5, 20, 5, 1, 0.9, 0.1, 1);
+	Monster player_test_2("Kakarott", 30, tmp, 2.5, 1);
+	
+	player_test_1.hit(&player_test_2);
+	
+	int test_hp = player_test_2.getHealthPoints();
+	int test_xp = player_test_1.getExperience();
 
+	int expected_hp = 27;
+	int expected_xp = 3;
 
-//Hero getCooldownMultiplierPerLevel
+	ASSERT_EQ(expected_hp, test_hp);
+	ASSERT_EQ(expected_xp, test_xp);
+}
 
+TEST(Hero, hit_w_levelup){
+	Damage tmp (3, 1);
+	Hero hero_test ("Prince Aidan of Khanduras", 30, tmp, 1.1, 0.5, 2, 5, 1, 0.9, 0.1, 1);
+	Monster monster_test ("Kakarott", 30, tmp, 2.5, 1);
+	
+	hero_test.hit(&monster_test);
+	
+	Damage test_dmg (hero_test.getDamage());
+	
+	int test_monster_hp = monster_test.getHealthPoints();
+	int test_hero_level = hero_test.getLevel();
+	int test_hero_mhp = hero_test.getMaxHealthPoints();
+	int test_hero_pdmg = test_dmg.getPhysical();
+	int test_hero_mdmg = test_dmg.getMagical();
+	int test_hero_hp = hero_test.getHealthPoints();
+	double test_hero_acd = hero_test.getAttackCoolDown();
+	double test_hero_def = hero_test.getDefense();
+	int test_hero_xp = hero_test.getExperience();
 
-//Hero getDefenseBonusPerLevel
+	int expected_monster_hp = 27;
+	int expected_hero_level = 2;
+	int expected_hero_mhp = 35;
+	int expected_hero_pdmg = 4;
+	int expected_hero_mdmg = 2;
+	int expected_hero_hp = 35;
+	double expected_hero_acd = 1.1*0.9;
+	double expected_hero_def = 0.6;
+	int expected_hero_xp = 1;
 
+	ASSERT_EQ(expected_monster_hp, test_monster_hp);
+	ASSERT_EQ(expected_hero_level, test_hero_level);
+	ASSERT_EQ(expected_hero_mhp, test_hero_mhp);
+	ASSERT_EQ(expected_hero_pdmg, test_hero_pdmg);
+	ASSERT_EQ(expected_hero_mdmg, test_hero_mdmg);
+	ASSERT_EQ(expected_hero_acd, test_hero_acd);
+	ASSERT_EQ(expected_hero_def, test_hero_def);
+	ASSERT_EQ(expected_hero_xp, test_hero_xp);
+}
 
-//Hero getMagicalDamageBonusPerLevel
+TEST(Hero, getHit){
+	Damage tmp (3, 1);
+	Hero player_test_1 ("Prince Aidan of Khanduras", 30, tmp, 1.1, 0.5, 20, 5, 1, 0.9, 0.1, 1);
+	Monster player_test_2("Kakarott", 30, tmp, 2.5, 1);
+	
+	player_test_2.getHit(&player_test_2);
+	
+	int test_hp = player_test_2.getHealthPoints();
+	
+	int expected_hp = 27;
+	
+	ASSERT_EQ(expected_hp, test_hp);
+}
 
-
-//Hero getHit ??
-
-
-//Hero hit
-
-
-*/
-
-//Hero parse
 TEST(Hero, parse){
 	Hero player_test = Hero::parse("../Dark_Wanderer.json");
+	
 	std::string test_name = player_test.getName();
 	int test_hp = player_test.getHealthPoints();
 	Damage test_dmg = player_test.getDamage();
@@ -302,7 +473,6 @@ TEST(Hero, parse){
 	ASSERT_EQ(expected_mdbpl, test_mdbpl);
 }
 
-
 TEST(Map, coordinateOutOfRange){
     Map map_test("../maps/level1.txt");
     
@@ -333,6 +503,7 @@ TEST(Damage, operatorPlus){
 TEST(Damage, operatorPlusEqual){
 	Damage test_dmg (100, 200);
 	Damage plus (10, 20);
+	
 	test_dmg += plus;
 	
 	int test_pdmg = test_dmg.getPhysical();
@@ -349,6 +520,7 @@ TEST(Damage, operatorPlusEqual){
 TEST(Damage, operatorMultiEqual){
 	Damage test_dmg (10, 20);
 	Damage multi (1, 2);
+	
 	test_dmg *= multi;
 	
 	int test_pdmg = test_dmg.getPhysical();
@@ -385,6 +557,7 @@ TEST(Damage, setPhysical){
 	Damage test_dmg (0, 0);
 	
 	test_dmg.setPhysical(10);
+	
 	int test_pdmg = test_dmg.getPhysical();
 
 	int expected_pdmg = 10;
@@ -396,6 +569,7 @@ TEST(Damage, setMagical){
 	Damage test_dmg (0, 0);
 	
 	test_dmg.setMagical(20);
+	
 	int test_mdmg = test_dmg.getMagical();
 
 	int expected_mdmg = 20;
