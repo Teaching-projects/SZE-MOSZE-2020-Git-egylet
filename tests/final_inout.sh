a832b321ef2b6707b8374cdce84192ca37828839
kokohun <natzkornel@gmail.com>
2020-12-07 16:44:46 +0100

    Refreshed final in-out test script

--- tests/final_inout.sh
+++ tests/final_inout.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-if test -f scenario1_native.txt
+if test -f scenario1_native_final.txt
 then
-	rm scenario1_native.txt
+	rm scenario1_native_final.txt
 fi
 
-if test -f scenario2_native.txt
-then
-	rm scenario2_native.txt
-fi
+cat units/directions.txt | ./a.out scenarios/scenario1.json >> native_output1_final.txt
 
-cat units/directions.txt | ./a.out scenarios/scenario1.json >> native_output1.txt
-cat units/directions.txt | ./a.out scenarios/scenario2.json >> native_output2.txt
-
-diff native_output1.txt scenarios/output1.txt
-diff native_output2.txt scenarios/output2.txt
+diff native_output1_final.txt scenarios/output1_final.txt
